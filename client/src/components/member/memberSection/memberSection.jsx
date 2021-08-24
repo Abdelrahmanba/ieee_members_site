d338a493100c83fc2d31cc3e63a38d9b4de7269e
Abdelrahman Baba <babaa.abd@gmail.com>
2021-08-24 16:56:06 +0300

    fix bugs

--- client/src/components/member/memberSection/memberSection.jsx
+++ client/src/components/member/memberSection/memberSection.jsx
@@ -1,12 +1,17 @@
+import { Divider } from 'antd'
 import './memberSection.styles.scss'
 
 const MemberSection = ({ title, children }) => {
   return (
     <section className='user-section'>
-      <h1>
-        {title.substring(0, title.lastIndexOf(' ') + 1)}
-        <span>{title.split(' ')[title.split(' ').length - 1]}</span>
-      </h1>
+      <div style={{ width: '70%' }}>
+        <Divider>
+          <h1>
+            {title.substring(0, title.lastIndexOf(' ') + 1)}
+            <span>{title.split(' ')[title.split(' ').length - 1]}</span>
+          </h1>
+        </Divider>
+      </div>
       {children}
     </section>
   )
